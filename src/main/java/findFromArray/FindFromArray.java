8fbe39222dc3a6188a469230e2f858f8aa06d939
Urmat Tilekov <urma.93.kg@mail.ru>
2020-07-14 20:25:12 +0600

    #Task 1 Find from array

---
+++ src/main/java/findFromArray/FindFromArray.java
@@ -0,0 +1,40 @@
+package findFromArray;
+
+public class FindFromArray {
+    public static void main(String[] args) {
+        //Дан массив из чисел:
+        int[] numbers = {2, 5, 6, 23, 3, 8, 22, 75, 4, -8};
+        System.out.println("Min: " + getMin(numbers));
+        System.out.println("Max: " + getMax(numbers));
+        System.out.println("Middle: " + getMiddle(numbers));
+    }
+
+    /**
+     * Возвращает среднее число из массива
+     * @param numbers массив чисел
+     * @return среднее число
+     */
+    private static int getMiddle(int[] numbers) {
+        return 0;
+    }
+
+    /**
+     * Возвращает наибольшее число из массива
+     * @param numbers массив чисел
+     * @return наибольшее число
+     */
+    private static int getMax(int[] numbers) {
+        return 0;
+    }
+
+    /**
+     * Возвращает наименьшее число из массива
+     * @param numbers массив чисел
+     * @return наименьшее число
+     */
+    private static int getMin(int numbers[]) {
+        return 0;
+    }
+
+
+}
