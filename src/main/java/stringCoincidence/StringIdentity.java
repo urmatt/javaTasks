d801c01ddc432b0f5cc33d56dacaf22faefbcc47
Urmat Tilekov <urma.93.kg@mail.ru>
2020-07-20 17:21:19 +0600

    #2 Count of identity in strings

---
+++ src/main/java/stringCoincidence/StringIdentity.java
@@ -0,0 +1,21 @@
+package stringCoincidence;
+
+public class StringIdentity {
+    public static void main(String[] args) {
+        String firstString = "First string";
+        String secondString = "Second string";
+
+        System.out.println("String coincidence: " + getIdentityCount(firstString, secondString));
+    }
+
+    /**
+     * Вычисляет количество идентичных букв в заданных строках
+     * Пример: в словах "телефон" и "телевизор" количество идентичности 5 букв
+     * @param firstString
+     * @param secondString
+     * @return процент идентичности
+     */
+    private static int getIdentityCount(String firstString, String secondString) {
+        return 0;
+    }
+}
