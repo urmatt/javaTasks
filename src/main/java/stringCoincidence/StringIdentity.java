59228d718558c6e04e5391c214b6f7aa08a48239
Urmat Tilekov <urma.93.kg@mail.ru>
2020-07-20 17:22:42 +0600

    #2 refactoring

--- src/main/java/stringCoincidence/StringIdentity.java
+++ src/main/java/stringCoincidence/StringIdentity.java
@@ -2,8 +2,8 @@ package stringCoincidence;
 
 public class StringIdentity {
     public static void main(String[] args) {
-        String firstString = "First string";
-        String secondString = "Second string";
+        String firstString = "First string to next";
+        String secondString = "Other string for first";
 
         System.out.println("String coincidence: " + getIdentityCount(firstString, secondString));
     }
@@ -11,6 +11,7 @@ public class StringIdentity {
     /**
      * Вычисляет количество идентичных букв в заданных строках
      * Пример: в словах "телефон" и "телевизор" количество идентичности 5 букв
+     *
      * @param firstString
      * @param secondString
      * @return процент идентичности
