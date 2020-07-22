16b15f81b1d84fa72630e07a33fe1d0049ef20be
Aigerim <aigerim.alymkulowa@gmail.com>
2020-07-22 12:23:17 +0600

    String identity

--- src/main/java/stringCoincidence/StringIdentity.java
+++ src/main/java/stringCoincidence/StringIdentity.java
@@ -1,5 +1,8 @@
 package stringCoincidence;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringIdentity {
     public static void main(String[] args) {
         String firstString = "First string to next";
@@ -17,6 +20,17 @@ public class StringIdentity {
      * @return процент идентичности
      */
     private static int getIdentityCount(String firstString, String secondString) {
-        return 0;
+        String first = firstString.toUpperCase();//FIRST ...
+        String second = secondString.toUpperCase();//OTHER ...
+        List<Character> foundChars = new ArrayList<>();
+        for (int i = 0; i < first.length(); i++) {
+            char ch = first.charAt(i);//S - 0
+            if (second.contains(String.valueOf(ch))) {
+                if (!foundChars.contains(ch) && ch != ' ') {//[FIRST ]
+                    foundChars.add(ch);
+                }
+            }
+        }
+        return foundChars.size();
     }
 }
